b5cabef3f23485f4e8e5c08883ac59dc5ad30502
NicolasSchwarzer <NicolasSchwarzer@users.noreply.github.com>
2015-04-14 22:00:37 +0800

    Initial Commit

---
+++ CardGameWNB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardGameWNB
+//
+//  Created by 万宁邦 on 15/4/14.
+//  Copyright (c) 2015年 万宁邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardGameWNB/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardGameWNB
+//
+//  Created by 万宁邦 on 15/4/14.
+//  Copyright (c) 2015年 万宁邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
